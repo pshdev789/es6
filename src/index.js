f9fd5b7d0800e262ef201392b3073280ef2a3460
pshdev789 <68139465+pshdev789@users.noreply.github.com>
2020-10-09 08:11:26 +0530

    Update index.js
    Initial commit with var let const changes.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// var -> function
+// let -> block
+// const -> allows only to read
+function sayHello(){
+const x=10;
+for(let i=0; i<5; i++){
+  var m=9;
+  console.log(i);
+}
+  // cannot reassign const
+  // x=9;
+  console.log("Value of var "+m);
+  console.log("Value of const "+x);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+sayHello();
