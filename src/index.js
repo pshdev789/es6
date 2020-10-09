2afb17f7fa1ee48032b697b549746db9218d39ab
pshdev789 <68139465+pshdev789@users.noreply.github.com>
2020-10-09 22:29:41 +0530

    Update index.js
    Objects and accessing fields
    Arrow functions

--- src/index.js
+++ src/index.js
@@ -12,6 +12,51 @@ for(let i=0; i<5; i++){
   console.log("Value of var "+m);
   console.log("Value of const "+x);
 
+
 }
 
 sayHello();
+
+ // Object
+
+ const person = {
+  name:'Kanchi',
+  //walk,talk are a method since it
+  // is a function inside a object
+  walk: function() {},
+  talk(){}
+ };
+
+//  Accessing Objects
+person.talk();
+person['name'] = 'Kanchika';
+
+// If know the property name ahead
+// can use . notation
+person.name;
+
+// If the name of the field is 
+// unknown use is like this with sqare bracets
+const targetMember = 'name';
+person[targetMember] = 'Kkk';
+
+
+// Arrow functions
+// Old js
+const square = function(number){
+  return number*number;
+}
+
+/**  Arrow fuction.
+ *If needed single parameter no need
+ * to use (), const square = number=>{}.
+ * no parameters use double braces, const square = ()=>{}
+ * If a one single line in the body ommit 
+ * 
+ * **/
+const square = number=>{
+  return number*number;
+}
+//  If a one single line in the body ommit {} and return keyword
+// number goes to number times number
+const square1 = number=> number*number;
